1637825224f4c5576468ce4c1de12baf51c98d0e
qxb650 <qxb650@alumni.ku.dk>
2024-04-02 17:42:03 +0200

    Hold

---
+++ dataproject/Countries.py
@@ -0,0 +1,31 @@
+# oecd
+oecd_countries = [
+    "AUS", "AUT","BEL", "CAN", "COL", "CHL", "CRI", "CZE","DNK", "EST","FIN", "FRA","DEU", "GRC",
+    "HUN", "ISL","IRL", "ISR","ITA", "JPN","KOR", "LVA","LTU","LUX", "MEX","NLD", 
+    "NZL","NOR","POL", "PRT","SVK", "SVN", "ESP","SWE", "CHE", "TUR", "GBR", "USA"
+    ]
+# imf advanced
+imf_advanced_countries = [
+    'AND', 'AUS', 'AUT', 'BEL', 'CAN', 'HRV', 'CYP', 'CZE', 'DNK', 'EST', 'FIN',
+    'FRA', 'DEU', 'GRC', 'HKG', 'ISL', 'IRL', 'ISR', 'ITA', 'JPN', 'KOR', 'LVA',
+    'LTU', 'LUX', 'MAC', 'MLT', 'NLD', 'NZL', 'NOR', 'PRT', 'PRI', 'SMR', 'SGP',
+    'SVK', 'SVN', 'ESP', 'SWE', 'CHE', 'TWN', 'GBR', 'USA'
+]
+# imf emerging
+imf_emerging_countries = [
+    'AFG', 'ALB', 'DZA', 'AGO', 'ATG', 'ARG', 'ARM', 'ABW', 'AZE', 'BHS', 'BHR',
+    'BGD', 'BRB', 'BLR', 'BLZ', 'BEN', 'BTN', 'BOL', 'BIH', 'BWA', 'BRA', 'BRN',
+    'BGR', 'BFA', 'BDI', 'CPV', 'KHM', 'CMR', 'CAF', 'TCD', 'CHL', 'CHN', 'COL',
+    'COM', 'COD', 'COG', 'CRI', "CIV", 'DJI', 'DMA', 'DOM', 'ECU', 'EGY', 'SLV',
+    'GNQ', 'ERI', 'SWZ', 'ETH', 'FJI', 'GAB', 'GMB', 'GEO', 'GHA', 'GRD', 'GTM',
+    'GIN', 'GNB', 'GUY', 'HTI', 'HND', 'HUN', 'IND', 'IDN', 'IRN', 'IRQ', 'JAM',
+    'JOR', 'KAZ', 'KEN', 'KIR', 'XKX', 'KWT', 'KGZ', 'LAO', 'LBN', 'LSO', 'LBR',
+    'LBY', 'MDG', 'MWI', 'MYS', 'MDV', 'MLI', 'MHL', 'MTQ', 'MRT', 'MUS', 'MEX',
+    'FSM', 'MDA', 'MNG', 'MNE', 'MAR', 'MOZ', 'MMR', 'NAM', 'NRU', 'NPL', 'NIC',
+    'NER', 'NGA', 'MKD', 'OMN', 'PAK', 'PLW', 'PAN', 'PNG', 'PRY', 'PER', 'PHL',
+    'POL', 'QAT', 'ROU', 'RUS', 'RWA', 'WSM', 'STP', 'SAU', 'SEN', 'SRB', 'SYC',
+    'SLE', 'SLB', 'SOM', 'ZAF', 'SSD', 'LKA', 'KNA', 'LCA', 'VCT', 'SDN', 'SUR',
+    'SYR', 'TJK', 'TZA', 'THA', 'TLS', 'TGO', 'TON', 'TTO', 'TUN', 'TUR', 'TKM',
+    'TUV', 'UGA', 'UKR', 'ARE', 'URY', 'UZB', 'VUT', 'VEN', 'VNM', 'PSE', 'YEM',
+    'ZMB', 'ZWE'
+]
